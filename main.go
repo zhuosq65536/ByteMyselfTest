93bd5d27b1103e70dbabe3e7fd3c197c85df2302
for shell <fromShell@paas.com>
2023-07-25 20:20:57 +0800

    加入随机数种子

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-  "math/rand"
-
+	"math/rand"
+	"time"
 )
 
 func main() {
   maxNum := 100
+  rand.Seed(time.Now().UnixNano())
   secretNumber := rand.Intn(maxNum);
   fmt.Println("The secretNumber is ",secretNumber)
 
